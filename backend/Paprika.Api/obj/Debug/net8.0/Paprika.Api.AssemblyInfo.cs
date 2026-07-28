8db9e904cf0261a9ea1a162abbd0d910f5adb8c0
Kamila <AliyaMarwa.Kamila@bakerhughes.com>
2026-07-28 07:58:16 +0700

    data routing 1 sbg contoh

--- backend/Paprika.Api/obj/Debug/net8.0/Paprika.Api.AssemblyInfo.cs
+++ backend/Paprika.Api/obj/Debug/net8.0/Paprika.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Paprika.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf1d956970881d3575c68d6309cf56bb7c11e76")]
 [assembly: System.Reflection.AssemblyProductAttribute("Paprika.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Paprika.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/eWorkOrder.API/obj/Debug/net8.0/eWorkOrder.API.AssemblyInfo.cs
+++ backend/eWorkOrder.API/obj/Debug/net8.0/eWorkOrder.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eWorkOrder.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e04fe8b42ac64267bbdc8518c6f3d83e8b62857")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf1d956970881d3575c68d6309cf56bb7c11e76")]
 [assembly: System.Reflection.AssemblyProductAttribute("eWorkOrder.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eWorkOrder.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
